88e9c424a7af4345136d5d67c456d3e9a4e68063
CXUtk <2237367067@qq.com>
2022-08-09 22:51:22 +0800

    Parameter fix

--- src/Graphics/Compute/ComputeParameter.cs
+++ src/Graphics/Compute/ComputeParameter.cs
@@ -147,20 +147,20 @@ namespace Microsoft.Xna.Framework.Graphics
 			{
 				float* dstPtr = (float*)floatBuf;
 				dstPtr[0] = value.M11;
-				dstPtr[1] = value.M21;
-				dstPtr[2] = value.M31;
-				dstPtr[3] = value.M41;
-				dstPtr[4] = value.M12;
+				dstPtr[1] = value.M12;
+				dstPtr[2] = value.M13;
+				dstPtr[3] = value.M14;
+				dstPtr[4] = value.M21;
 				dstPtr[5] = value.M22;
-				dstPtr[6] = value.M32;
-				dstPtr[7] = value.M42;
-				dstPtr[8] = value.M13;
-				dstPtr[9] = value.M23;
+				dstPtr[6] = value.M23;
+				dstPtr[7] = value.M24;
+				dstPtr[8] = value.M31;
+				dstPtr[9] = value.M32;
 				dstPtr[10] = value.M33;
-				dstPtr[11] = value.M43;
-				dstPtr[12] = value.M14;
-				dstPtr[13] = value.M24;
-				dstPtr[14] = value.M34;
+				dstPtr[11] = value.M34;
+				dstPtr[12] = value.M41;
+				dstPtr[13] = value.M42;
+				dstPtr[14] = value.M43;
 				dstPtr[15] = value.M44;
 			}
 			FNA3D.FX11_Effect_Variable_SetMatrix4f(variablePtr, floatBuf);
